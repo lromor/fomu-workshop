22d893e20ab49e8bcde5e5f0be65189e39689bdb
Leonardo Romor <leonardo.romor@gmail.com>
2020-02-07 17:48:24 +0100

    Added the two registers in ram example

---
+++ litex/examples/two_registers/two_registers.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+# This variable defines all the external programs that this module
+# relies on.  lxbuildenv reads this variable in order to ensure
+# the build will finish without exiting due to missing third-party
+# programs.
+LX_DEPENDENCIES = ["icestorm", "yosys", "nextpnr-ice40"]
+#LX_CONFIG = "skip-git" # This can be useful for workshops
+
+# Import lxbuildenv to integrate the deps/ directory
+import os,os.path,shutil,sys,subprocess
+print(os.path.abspath(os.path.dirname(__file__)+ '../../'))
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)+ '../../'))
+import lxbuildenv
+
+# Disable pylint's E1101, which breaks completely on migen
+#pylint:disable=E1101
+
+from migen import *
+from migen.genlib.resetsync import AsyncResetSynchronizer
+
+from litex.soc.integration.soc_core import SoCCore
+from litex.soc.integration.builder import Builder
+from litex.soc.interconnect.csr import AutoCSR, CSRStatus, CSRStorage
+
+from litex_boards.partner.targets.fomu import BaseSoC, add_dfu_suffix
+
+from valentyusb.usbcore import io as usbio
+from valentyusb.usbcore.cpu import dummyusb
+
+import argparse
+
+class TwoRegModule(Module, AutoCSR):
+    def __init__(self):
+        self.foo = CSRStorage(32)
+        self.bar = CSRStorage(32)
+        # We store it into sync, the memory access is synchronous
+        # i.e. it needs a clock.
+        self.sync += self.bar.storage.eq(self.foo.storage)
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Build Fomu Main Gateware")
+    parser.add_argument(
+        "--seed", default=0, help="seed to use in nextpnr"
+    )
+    parser.add_argument(
+        "--placer", default="heap", choices=["sa", "heap"], help="which placer to use in nextpnr"
+    )
+    parser.add_argument(
+        "--board", choices=["evt", "pvt", "hacker"], required=True,
+        help="build for a particular hardware board"
+    )
+    args = parser.parse_args()
+
+    soc = BaseSoC(args.board, pnr_seed=args.seed, pnr_placer=args.placer, usb_bridge=True)
+
+    # Add our own module.
+    soc.submodules.two_reg_mod = TwoRegModule()
+    soc.add_csr("two_reg_mod")
+
+    builder = Builder(soc,
+                      output_dir="build", csr_csv="build/csr.csv",
+                      compile_software=False)
+    vns = builder.build()
+    soc.do_exit(vns)
+    add_dfu_suffix(os.path.join('build', 'gateware', 'top.bin'))
+
+
+if __name__ == "__main__":
+    main()
